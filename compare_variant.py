d7cb1b9804cfd77fa5b79f0fc1d2ebc7e9972b45
johan Rollin <johan.rollin.sfr@hotmail.fr>
2021-02-18 18:36:05 +0100

    Update compare_variant.py

--- compare_variant.py
+++ compare_variant.py
@@ -7,28 +7,64 @@ import argparse
 import os
 import pandas as pd
 import statistics
-
-def open_file(out_dir, file_name_data, file_name_control, col_name, col_name_control):
+def clean_data(new_df, clean_virus_data):
     """
     """
-    try:
-        virus_data = pd.read_csv(os.path.join(out_dir, file_name_data),
-                                sep=";", header=0, names=col_name,
-                                index_col=False)
-    except pd.errors.ParserError:
-        virus_data = pd.read_csv(os.path.join(out_dir, file_name_data),
-                                sep=",", header=0, names=col_name,
-                                index_col=False)        
-    try:
-        control_data = pd.read_csv(os.path.join(out_dir, file_name_control),
-                        sep=";", header=0, names=col_name_control,
-                        index_col=False)
-    except pd.errors.ParserError:
-        control_data = pd.read_csv(os.path.join(out_dir, file_name_control),
-                        sep=",", header=0, names=col_name_control,
-                        index_col=False)
+    col_name = ["Lab", "File_name", "Input_file", "Org_ref", "Position", \
+        "Type", "Reference","Allele", "Coverage", "Average_quality", \
+        "Variant_frequency", "Personal_validation", "Comment"]
+    clean_virus_tmp = pd.DataFrame(columns=col_name)
+    clean_virus_list = []
+    
+    # print(new_df.loc[new_df.index[pd.isna(new_df["File name"])]])
+    dumb_list1 = []
+    dumb_list2 = []
+    dumb_list3 = []
 
-    return virus_data, control_data
+    # remove row when "File name" is null
+    update_df = new_df.drop(new_df.index[pd.isna(new_df["File name"])])
+
+    for index, row in update_df.iterrows():
+        el = row["File name"]
+        data = el.split("_")
+        input_file_data = str(data[0] + "_" + data[1])
+        org_ref_data = data[2]
+        if "." in org_ref_data:
+            org_ref_data = org_ref_data.split(".")[0]
+        file_name = str(input_file_data + "_" + org_ref_data )
+        update_df.loc[index,"Input_file"] = input_file_data
+        update_df.loc[index,"Org_ref"] = org_ref_data
+        update_df.loc[index,"File_name"] = file_name
+        dumb_list1.append(el)
+        dumb_list2.append(input_file_data)
+        dumb_list3.append(org_ref_data)
+    
+    update_df = update_df.drop(["File name"], axis=1)
+    # print("File name")
+    # print(set(dumb_list1))  
+    # print("input_file_data")
+    # print(set(dumb_list2))   
+    # print("org_ref_data")
+    # print(set(dumb_list3))    
+    clean_virus_data = clean_virus_data.append(update_df, ignore_index=True)
+    # print(clean_virus_data)
+
+    return clean_virus_data
+
+def open_file(full_path, col_name, lab_dir, clean_virus_data):
+    """
+    """
+    # print(full_path)
+    xls = pd.ExcelFile(full_path, engine='openpyxl')
+    sheet_list = xls.sheet_names
+    for sheet in sheet_list:
+        # print(sheet)
+        virus_data = pd.read_excel(full_path, engine='openpyxl', sheet_name=sheet)
+        virus_data.insert(0,"Lab", lab_dir)
+        selected_columns = virus_data[col_name]
+        new_df = selected_columns.copy()
+        clean_virus_data = clean_data(new_df, clean_virus_data)
+    return clean_virus_data
 
 
 # def write_result(out_dir, file_name_data, virus_data, t1_threshold, 
@@ -70,27 +106,27 @@ def open_file(out_dir, file_name_data, file_name_control, col_name, col_name_con
 if __name__ == "__main__":
 
     #TODO argparse use argument ?
-    col_name = ["File name","Type","Reference","Allele", "Coverage", "Average quality", \
-    "Variant Frequency", "Personal validation", "Comment"] 
+    col_name = ["Lab", "File name", "Position", \
+        "Type", "Reference","Allele", "Coverage", "Average quality", \
+        "Variant Frequency", "Personal validation", "Comment"]
 
     out_dir = "/mnt/c/Users/johan/OneDrive/Bureau/bioinfo/PT3/Result/variant_calling/"
-    
-    file_name_control = "Input_file_control_3_20012021.csv"
-    file_name_data = "Tested_virus_file_3_20012021.csv"
+    clean_virus_data = pd.DataFrame(columns=col_name)
     
     for lab_dir in os.listdir(out_dir):
-        print(lab_dir)
+        #print(lab_dir)
         filename_list = os.listdir(os.path.join(out_dir, lab_dir))
         for filename in filename_list:
             if filename.startswith("PT3_snp_template"):
-                print(filename)
-                #virus_data = open_file(out_dir, file_name_data, file_name_control, col_name, col_name_control)
-
-        #  and ext == '.py':
-        #     print filename
-
+                #print(filename)
+                full_path = os.path.join(os.path.join(out_dir, lab_dir),filename)
+                clean_virus_data = open_file(full_path, col_name, lab_dir, clean_virus_data)
+    print('toto')
+    print(clean_virus_data)
     #filename.startswith("")
     
-    
+    print(clean_virus_data.File_name.unique())
+    print(clean_virus_data.Org_ref.unique())
+    print(clean_virus_data.Input_file.unique())
     # open input file
     #virus_data, control_data = open_file(out_dir, file_name_data, file_name_control, col_name, col_name_control)
